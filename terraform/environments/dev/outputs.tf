106083fe3e4651280368579032294f598bc224d2
Gagan Chaudhary <gagan.chaudhary@kickdrumtech.com>
2025-03-13 12:22:28 +0530

    workflows

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -3,16 +3,6 @@ output "website_url" {
   description = "The CloudFront URL of the website"
 }
 
-output "s3_bucket_name" {
-  value       = module.s3_website.bucket_name
-  description = "The name of the S3 bucket hosting the website content"
-}
-
-output "cloudfront_distribution_id" {
-  value       = module.cloudfront.cloudfront_distribution_id
-  description = "The ID of the CloudFront distribution"
-}
-
 output "state_bucket_name" {
   value       = var.create_state_resources ? module.state[0].bucket_id : var.state_bucket_name
   description = "The name of the S3 bucket for terraform state"
