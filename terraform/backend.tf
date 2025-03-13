d7f6817ab60cbacd0bd3823115f7106d0cc10c67
Gagan Chaudhary <gagan.chaudhary@kickdrumtech.com>
2025-03-13 12:16:58 +0530

    workflows

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rizzlers-terraform-state"
+    bucket         = "rizzlers-ibe-dev-tfstate"
     key            = "frontend/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -75,14 +75,4 @@ variable "create_state_resources" {
 output "website_url" {
   value       = "https://${module.cloudfront.cloudfront_domain_name}"
   description = "The CloudFront URL of the website"
-}
-
-output "s3_bucket_name" {
-  value       = module.s3_website.bucket_name
-  description = "The name of the S3 bucket hosting the website content"
-}
-
-output "cloudfront_distribution_id" {
-  value       = module.cloudfront.cloudfront_distribution_id
-  description = "The ID of the CloudFront distribution"
 } 
